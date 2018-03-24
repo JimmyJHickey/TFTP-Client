78a908ff5e73228a4444dfe3cee560faf06fc802
Jimmy Hickey <jimmyjhickey@gmail.com>
2018-03-24 14:36:24 -0500

    Added opcode and mode constants

--- src/Const.java
+++ src/Const.java
@@ -10,4 +10,17 @@ public class Const
 	final static int TFTP_PORT = 69;
 	final static int MAX_PORT = 65535;
 	final static int RESERVED_PORTS = 1024;
+	final static int PACKET_SIZE = 512;
+	final static byte TERM = 0;
+	
+	// opcodes
+	final static byte RRQ = 1;
+	final static byte WRQ = 2;
+	final static byte DATA = 3;
+	final static byte ACK = 4;
+	final static byte ERROR = 5;
+
+	// Modes
+	final static String NETASCII = "netascii";
+	final static String OCTET = "octet";
 }
